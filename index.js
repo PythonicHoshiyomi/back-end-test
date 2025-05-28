e49faf3d347a3f2e5f54000a9ea29f8da4aa6a86
PythonicHoshiyomi <gizajirndhara@gmail.com>
2025-05-28 11:50:04 +0700

    final commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Import required modules and initialize environment variables
-const { PrismaClient } = require('./generated/prisma');
 const express = require('express');
 const dotenv = require('dotenv');
+const userController = require('./routes/user_controller');
 
 dotenv.config();
 
-// Initialize Prisma Client and Express app
-const prisma = new PrismaClient();
+// Initialize Express app
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -24,51 +23,5 @@ app.get('/', async (req, res) => {
     console.log('hello world');
 });
 
-// Get all users
-app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.send(users);
-});
-
-// Create a new user
-app.post('/users', async (req, res) => {
-    const newProduct = req.body;
-    const newUser = await prisma.user.create({
-        data: {
-            name: newProduct.name,
-            email: newProduct.email,
-            password: newProduct.password,
-        }
-    });
-
-    res.status(201).send(newUser);
-});
-
-// Delete a user by ID
-app.delete('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedUser = await prisma.user.delete({
-        where: {
-            id: Number(id),
-        }
-    });
-
-    res.status(200).send(deletedUser);
-});
-
-// Update a user by ID
-app.put('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const updatedUser = await prisma.user.update({
-        where: {
-            id: Number(id),
-        },
-        data: {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        }
-    });
-
-    res.status(200).send(updatedUser);
-});
+// Use userController for /users routes
+app.use('/users', userController);
